8dab8f9b5e6b5f833f69f44a8740268684797140
Michael Madell <vf9z59xqj2qfuoehba19b0f5pbu2ba@bots.bitbucket.org>
2026-03-16 11:39:57 +0000

    Update version.h to 20.26.3.1_rc1

--- src/version.h
+++ src/version.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #define VERSION_MAJOR 20
-#define VERSION_MINOR 25
-#define VERSION_RELEASE 11
+#define VERSION_MINOR 26
+#define VERSION_RELEASE 3
 #define VERSION_BUILD 1
 #define VERSION_EXTRAVERSION "rc"
-#define VERSION_RC_NO 4
-#define VERSION_ADHOC_NO 4
+#define VERSION_RC_NO 1
+#define VERSION_ADHOC_NO 1
 
 #define SERIAL_PORT "\\\\.\\COM3"
 
