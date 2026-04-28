a57557ce872b1a692f7e114273b48d2f688aad69
Michael Madell <vf9z59xqj2qfuoehba19b0f5pbu2ba@bots.bitbucket.org>
2026-04-28 08:41:31 +0100

    update versioning for 20.26.5.1_rc1

--- src/version.h
+++ src/version.h
@@ -2,11 +2,11 @@
 
 #define VERSION_MAJOR 20
 #define VERSION_MINOR 26
-#define VERSION_RELEASE 3
+#define VERSION_RELEASE 5
 #define VERSION_BUILD 1
-#define VERSION_EXTRAVERSION "ga"
-#define VERSION_RC_NO 2
-#define VERSION_ADHOC_NO 2
+#define VERSION_EXTRAVERSION "rc"
+#define VERSION_RC_NO 1
+#define VERSION_ADHOC_NO 1
 
 #define SERIAL_PORT "\\\\.\\COM3"
 
